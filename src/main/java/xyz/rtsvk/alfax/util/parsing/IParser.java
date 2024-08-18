f14ebde7548dba0be9ac25b37c8bd546c58f81f3
Jastrobaron <kubo.hresko@gmail.com>
2024-08-18 12:24:37 +0200

    squash me

--- src/main/java/xyz/rtsvk/alfax/util/parsing/IParser.java
+++ src/main/java/xyz/rtsvk/alfax/util/parsing/IParser.java
@@ -6,11 +6,12 @@ import java.util.Map;
  * Generic parser interface
  * @author Jastrobaron
  */
-public interface AParser {
+public interface IParser {
 	/**
 	 * Method to parse into a key-value map
 	 * @param source of raw non-parsed data
 	 * @return data parsed into a key-value map
+	 * @throws Exception when an error occurs during parsing
 	 */
-	Map<String, Object> parse(Object source);
+	Map<String, Object> parse(Object source) throws Exception;
 }
--- src/main/java/xyz/rtsvk/alfax/util/parsing/json/JsonParser.java
+++ src/main/java/xyz/rtsvk/alfax/util/parsing/json/JsonParser.java
@@ -2,20 +2,33 @@ package xyz.rtsvk.alfax.util.parsing.json;
 
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import xyz.rtsvk.alfax.webserver.IContent;
+import xyz.rtsvk.alfax.util.parsing.IParser;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.util.Map;
 
-public class JsonContent implements IContent {
+/**
+ * Class used for parsing JSON data
+ * @author Jastrobaron
+ */
+public class JsonParser implements IParser {
+
+	private static final JSONParser parser = new JSONParser();
+
 	@Override
-	public Map<String, Object> parse(String s) {
+	public Map<String, Object> parse(Object source) {
 		try {
-			JSONParser parser = new JSONParser();
-			return (Map<String, Object>) parser.parse(s);
-		}
-		catch (ParseException e) {
+			if (source instanceof String rawData) {
+				return (Map<String, Object>) parser.parse(rawData);
+			} else if (source instanceof Reader in) {
+				return (Map<String, Object>) parser.parse(in);
+			} else {
+				return Map.of();
+			}
+		} catch (IOException | ParseException e) {
 			e.printStackTrace();
-			return null;
+			return Map.of();
 		}
 	}
 }
